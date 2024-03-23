e42ab5996428530d23b870ef110035c2fdea0fa0
kangwongyu795 <108566980+kangwongyu795@users.noreply.github.com>
2024-03-23 09:01:30 +0900

    오류 발생으로 인한 커밋

--- V-Search/src/main/java/com/mysite/sbb/member/MemberRepository.java
+++ V-Search/src/main/java/com/mysite/sbb/member/MemberRepository.java
@@ -1,6 +1,5 @@
 package com.mysite.sbb.member;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
