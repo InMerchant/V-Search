3a73dd1a8377059ec0214a41adec26d273be20bc
kangwongyu795 <108566980+kangwongyu795@users.noreply.github.com>
2024-04-04 14:33:35 +0900

    회원탈퇴 기능 일부 수정

--- V-Search/src/main/java/com/mysite/sbb/Withdrawal/WithdrawalController.java
+++ V-Search/src/main/java/com/mysite/sbb/Withdrawal/WithdrawalController.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -14,23 +15,28 @@ public class WithdrawalController {
     @Autowired
     private WithdrawalService withdrawalService;
 
-    @GetMapping("/withdraw") // GET 요청 처리
+    @GetMapping("/withdraw")
     public String showWithdrawalPage() {
-        return "withdrawal"; // 회원 탈퇴 페이지로 이동
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.isAuthenticated()) {
+            return "withdrawal"; // 회원 탈퇴 페이지로 이동
+        } else {
+            // 사용자가 인증되지 않은 경우, 로그인 페이지로 리다이렉트
+            return "redirect:/login";
+        }
     }
 
     @PostMapping("/withdraw")
-    public String withdrawMember(RedirectAttributes redirectAttributes) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String username = auth.getName(); // 현재 사용자의 이름 가져오기
-
-        boolean isWithdrawn = withdrawalService.withdrawMember(username);
+    public String withdrawMember(@RequestParam("username") String username,
+                                 @RequestParam("password") String password,
+                                 RedirectAttributes redirectAttributes) {
+        boolean isWithdrawn = withdrawalService.withdrawMember(username, password);
         if (isWithdrawn) {
             redirectAttributes.addFlashAttribute("successMessage", "회원 탈퇴가 완료되었습니다.");
-            return "redirect:/"; // 로그아웃 페이지로 리다이렉트
+            return "redirect:/";
         } else {
             redirectAttributes.addFlashAttribute("errorMessage", "회원 탈퇴에 실패했습니다. 다시 시도해주세요.");
-            return "redirect:/userpage"; // 사용자 페이지로 리다이렉트
+            return "redirect:/userpage"; 
         }
     }
 }
