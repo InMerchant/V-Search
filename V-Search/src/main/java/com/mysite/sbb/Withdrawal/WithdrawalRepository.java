60a6df9f40c7912e92aa2b53f493e3c5fce6bc87
leekj601 <leekj601@naver.com>
2024-04-05 10:25:28 +0900

    메인 페이지 수정중...

--- V-Search/src/main/java/com/mysite/sbb/Withdrawal/WithdrawalRepository.java
+++ V-Search/src/main/java/com/mysite/sbb/Withdrawal/WithdrawalRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WithdrawalRepository extends JpaRepository<Withdrawal, Long> {
+public interface WithdrawalRepository extends JpaRepository<Withdrawal, String> {
     Withdrawal findByUserName(String username);
 }
