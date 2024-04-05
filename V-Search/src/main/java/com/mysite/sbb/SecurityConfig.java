cf87d6a35fa2dce06949d9b08e0f658b6b33b825
kangwongyu795 <108566980+kangwongyu795@users.noreply.github.com>
2024-04-05 14:25:07 +0900

    일부 수정

--- V-Search/src/main/java/com/mysite/sbb/SecurityConfig.java
+++ V-Search/src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.mysite.sbb;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,7 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
+
 
 
 
@@ -25,10 +24,8 @@ public class SecurityConfig {
         http
             .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                 .requestMatchers(new AntPathRequestMatcher("/withdraw")).authenticated() // /withdraw에 대한 접근 권한을 인증된 사용자로 설정
-                .requestMatchers(new AntPathRequestMatcher("/uploadForm")).authenticated() // /withdraw에 대한 접근 권한을 인증된 사용자로 설정
-                
-                .requestMatchers(new AntPathRequestMatcher("/Member/**")).authenticated()
-
+                .requestMatchers(new AntPathRequestMatcher("/uploadForm")).authenticated() // /uploadForm에 대한 접근 권한을 인증된 사용자로 설정
+                .requestMatchers(new AntPathRequestMatcher("/Member/**")).authenticated() //userpage에 대한 접근 권한을 인증된 사용자로 설정
                 .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()) // 나머지 모든 요청은 모든 사용자에게 허용
             .csrf().disable()
             .headers((headers) -> headers
