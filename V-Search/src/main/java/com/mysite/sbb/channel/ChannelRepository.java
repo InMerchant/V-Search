855b55693fc5d8f804c567118b6246d0f0ecadf9
배상인 <106319540+InMerchant@users.noreply.github.com>
2024-04-12 15:34:59 +0900

    채널 jpa 수정

--- V-Search/src/main/java/com/mysite/sbb/channel/ChannelRepository.java
+++ V-Search/src/main/java/com/mysite/sbb/channel/ChannelRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChannelRepository extends JpaRepository<Channel, Integer> {
+public interface ChannelRepository extends JpaRepository<Channel, String> {
     List<Channel> findByUserNumber(int userNumber);
 }
