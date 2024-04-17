295005d44a70ac0be78d5289ab6912e8bef59217
leekj601 <leekj601@naver.com>
2024-04-17 11:08:31 +0900

    수정 버젼

--- V-Search/src/main/java/com/mysite/sbb/userpage/UserPageController.java
+++ V-Search/src/main/java/com/mysite/sbb/userpage/UserPageController.java
@@ -6,23 +6,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.mysite.sbb.User.UserService;
+
 @Controller
 public class UserPageController {
 
-    @Autowired
-    private UserPageService userPageService;
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private UserPageService userPageService;
 
-    @GetMapping("/Member/{username}")
-    public String getUserPage(@PathVariable("username") String username, Model model) {
-        UserPage user = userPageService.getUserByUsername(username);
-        if (user != null) {
-            model.addAttribute("user", user);
-            return "userpage"; // userpage.html을 렌더링하는 뷰의 이름
-        } else {
-            // 사용자를 찾을 수 없는 경우 처리
-            return "userNotFound";
-        }
-    }
+	@GetMapping("/Member/{username}")
+	public String getUserPage(@PathVariable("username") String username, Model model) {
+		UserPage user = userPageService.getUserByUsername(username);
+		int userNo = userService.getUserNO(username);
+		if (user != null) {
+			model.addAttribute("userNo",userNo);
+			model.addAttribute("user", user);
+			return "userpage"; // userpage.html을 렌더링하는 뷰의 이름
+		} else {
+			// 사용자를 찾을 수 없는 경우 처리
+			return "userNotFound";
+		}
+	}
 
 }
- 
