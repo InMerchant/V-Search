55925226c713ab0feb7293d030118f660658523c
kangwongyu795 <108566980+kangwongyu795@users.noreply.github.com>
2024-05-10 10:00:11 +0900

    추천 기능 재구성

--- V-Search/src/main/java/com/mysite/sbb/recommend/Recommend.java
+++ V-Search/src/main/java/com/mysite/sbb/recommend/Recommend.java
@@ -6,6 +6,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.SequenceGenerator;
 @Data
 @Entity
 @Table(name = "RECOMMEND", schema = "bae")
@@ -15,10 +18,19 @@ public class Recommend {
     @Column(name = "VIDEO_NO")
     private int videoNo;
     
-    @Id
+    
     @Column(name = "USER_NO")
     private int userNo; //
 
     @Column(name = "RECOMMEND_CHK")
     private boolean recommendCheck;
+    
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "recommend_id_seq")
+    @SequenceGenerator(name = "recommend_id_seq", sequenceName = "RECOMMEND_ID_SEQ", allocationSize = 1)
+    @Column(name ="ID")
+    private int id;
+    
 }
+
