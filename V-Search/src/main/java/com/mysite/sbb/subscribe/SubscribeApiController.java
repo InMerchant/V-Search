fbbaa371a46b50fd68ca5c28c8bd5cfc3f8ad8ee
배상인 <106319540+InMerchant@users.noreply.github.com>
2024-06-13 12:38:39 +0900

    구독기능 수정

--- V-Search/src/main/java/com/mysite/sbb/subscribe/SubscribeApiController.java
+++ V-Search/src/main/java/com/mysite/sbb/subscribe/SubscribeApiController.java
@@ -24,23 +24,24 @@ public class SubscribeApiController {
 	private SubscribeService subscribeService;
 
 	@PostMapping("/api/subscribe/{videoNo}")
-	public String subscribe(@PathVariable("videoNo") int videoNo) {
+	public ResponseEntity<?> subscribe(@PathVariable("videoNo") int videoNo) {
 		String username = SecurityContextHolder.getContext().getAuthentication().getName();
 		int principalNo = userService.getUserNO(username);
 		Del video = entityManager.find(Del.class, videoNo);
 		int videoUserId = video.getUsernumber();
 		subscribeService.subscribe(principalNo, videoUserId, videoNo);
 
-		return "redirect:/video/{videoNo}";
+		return ResponseEntity.ok("Subscribed successfully");
 	}
 
-	@GetMapping("/api/subscribe/{videoNo}")
-	public String unsubscribe(@PathVariable("videoNo") int videoNo) {
+	@DeleteMapping("/api/subscribe/{videoNo}")
+	public ResponseEntity<?> unsubscribe(@PathVariable("videoNo") int videoNo) {
 		String username = SecurityContextHolder.getContext().getAuthentication().getName();
 		int principalNo = userService.getUserNO(username);
 		Del video = entityManager.find(Del.class, videoNo);
-		int videoUserId=video.getUsernumber();
+		int videoUserId = video.getUsernumber();
 		subscribeService.unSubscribe(principalNo, videoUserId);
-		return "redirect:/video/{videoNo}";
+
+		return ResponseEntity.ok("Unsubscribed successfully");
 	}
 }
